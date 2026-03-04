ae6644152201f5c8ee9b7fc92182b72fc2b46332
Shiruvi <Shiruvireu@gmail.com>
2026-03-04 20:05:45 +0300

    configuring fish

--- home/Shiruvi/store/files.nix
+++ home/Shiruvi/store/files.nix
@@ -3,11 +3,13 @@
     ".config/nvim".source = ./nvim;
     ".config/OpenTabletDriver".source = ./OpenTabletDriver;
     ".config/btop".source = ./btop;
-    ".config/fish".source = ./fish;
     ".config/fuzzel".source = ./fuzzel;
     ".config/kitty".source = ./kitty;
     ".config/mako".source = ./mako;
-    ".config/nautilus".source = ./nautilus;
     ".config/niri".source = ./niri;
+
+
+
+    ".config/fish/config.fish".source = ./fish/config.fish;
   };
 }
