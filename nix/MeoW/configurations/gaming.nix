caf1729d3b0317536462692d1d5dfa4e0baebf9c
Shiruvi <Shiruvireu@gmail.com>
2026-03-07 13:12:13 +0300

    screen br

--- nix/MeoW/configurations/gaming.nix
+++ nix/MeoW/configurations/gaming.nix
@@ -1,5 +1,12 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.steam = {
     enable = true;
   };
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+  ];
 }
--- nix/Nya/configurations/gaming.nix
+++ nix/Nya/configurations/gaming.nix
@@ -1,6 +1,12 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.steam = {
     enable = true;
   };
-  programs.gamescope.enable = true;
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+  ];
 }
